c13d22b9c2800bae81e756dc822a6975e2bc5b04
AaronSotoPacheco <111542116+AaronSotoPacheco@users.noreply.github.com>
2025-11-05 13:22:00 -0700

    hola mundo prolog

---
+++ 3_prolog/holamundo.pl
@@ -0,0 +1,16 @@
+valioso(oro).
+valioso(plata).
+le_gusta(juan,maria).
+tiene(juan,libro).
+tiene(maria,libro).
+
+%Diagrama_Genealogico
+progenitor(clara,jose).
+progenitor(tomas, jose).
+progenitor(tomas, isabel).
+progenitor(jose, ana).
+progenitor(jose, patricia).
+progenitor(patricia, jaime).
+hijo(isabel,tomas).
+
+%progenitor(X, ana),progenitor(Y,X), hijo(Z,Y).
